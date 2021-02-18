bfc4099c357e26e9b7eb9e6712997c0ca441450c
kumo <2732946269@qq.com>
2021-02-18 15:42:09 +0800

    Site updated: 2021-02-18 15:42:08

--- dadengtizi/src/extraSettings.js
+++ dadengtizi/src/extraSettings.js
@@ -35,7 +35,7 @@ let fruitSlowDown = false;
 let clickChangeFruit = false;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
-let adLink = 'https://space.bilibili.com/87828806/favlist';
+let adLink = '';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成大灯蹄子';
